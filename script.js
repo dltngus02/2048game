b9c1488d996aaa77224f25486d5a51a0ac68424a
이수현 <120762921+dltngus02@users.noreply.github.com>
2024-06-23 16:19:05 +0900

    이동부분 구현

--- script.js
+++ script.js
@@ -1,30 +1,17 @@
-let tableData = [
+
+const tableData = [
     ["", "", "", ""],
     ["", "", "", ""],
     ["", "", "", ""],
     ["", "", "", ""]
 ];
-const randomLocation = () => {
-    var x = Math.floor(Math.random() * 4);
-    var y = Math.floor(Math.random() * 4);
-    return [x, y];
-  };
+let score = 0
+let best = 0
 
-const existLocation = (tableData) => {  //중복확인하고 나온값을 테이블에 집어 넣기
-    var [x,y] = randomLocation()
-        while(tableData[y][x]){
-            [x,y] = randomLocation();
-        }
-        tableData[y][x] = randomNumber();
-        return [x,y];
-    }
-
-const randomNumber = () => {
-    return (Math.floor(Math.random() * 8) ===1) ? 4 : 2
-  };
 
 // 왼쪽으로 움직일 수 있는지 확인하는 함수
 const canMoveLeft = (tableData) => {
+    
     for (let y = 0; y < 4; y++) {
       for (let x = 1; x < 4; x++) {
         if (tableData[y][x]){ //문자열은 참값
@@ -73,9 +60,7 @@ const canMoveUp = (tableData) => {
     }
     return false;
 };
-const canMoveDown = (tableData) => {
-    //아래쪽으로 움직일 수 있는지 체크하는 함수
-  
+const canMoveDown = (tableData) => {//아래쪽으로 움직일 수 있는지 체크하는 함수
     for (let y = 0; y < 3; y++) {
       for (let x = 0; x < 4; x++) {
         if (tableData[y][x]){
@@ -84,9 +69,7 @@ const canMoveDown = (tableData) => {
                 return true;
             }
             if (tableData.slice(y).some(row => row[x] === "")) {
-                if (tableData[downy][x] === "") {
                     return true;
-                }
             }
         }
       }
@@ -94,10 +77,33 @@ const canMoveDown = (tableData) => {
     return false;
   };
 
+
+const randomLocation = () => { //위치 뽑는 함수 
+    var x = Math.floor(Math.random() * 4);
+    var y = Math.floor(Math.random() * 4);
+    return [x, y];
+  };
+
+const existLocation = (tableData) => {  //중복확인하고 나온값을 테이블에 집어 넣기
+    var [x,y] = randomLocation()
+        while(tableData[y][x]){
+            [x,y] = randomLocation();
+        }
+        tableData[y][x] = randomNumber();
+        return [x,y];
+    }
+
+const randomNumber = () => { //0.125퍼의 확률로 4뽑는 함수 
+    return (Math.floor(Math.random() * 8) ===1) ? 4 : 2
+  };
+
+
 const resetConfirm = (tableData) => {
     var confirmflag = confirm("Are you sure you want to start a new game? All progress will be lost.");
     if(confirmflag){
+        score = 0
         resetGame(tableData)
+        
   }
 }
 const resetGame = (tableData) => { //resetbutton 눌렀을 시 동작하는 함수
@@ -111,26 +117,82 @@ const resetGame = (tableData) => { //resetbutton 눌렀을 시 동작하는 함�
     renderTable(tableData)
 
 }
-const renderTable = (data) => {//gametable을 다시 그려주는 함수
+const renderTable = (tableData) => {//gametable을 다시 그려주는 함수
     const table = document.getElementById('gameTable');
     table.innerHTML = '';
-    data.forEach(rowData => {
+    tableData.forEach(rowData => {
         const tr = createTr(rowData);
         table.appendChild(tr);
     });
 };
 
-function createTd(cellData) { //칸을 그려주는 함수 
+const createTd = (cellData)  => { //칸을 그려주는 함수 
     const td = document.createElement('td');
     const div = document.createElement('div');
     div.className = 'grid-cell';
     div.textContent = cellData;
+    if(cellData>=8){
+        div.style.color = 'white';
+    }
+
+    if (cellData < 10) {
+        div.style.fontSize = '55px'; // 예시: 길이가 1일 때
+    } else if (cellData < 100) {
+        div.style.fontSize = '52px'; // 예시: 길이가 2일 때
+    } else if (cellData < 1000) {
+        div.style.fontSize = '49px'; // 예시: 길이가 3일 때
+    } else if (cellData < 10000) {
+        div.style.fontSize = '46px'; // 예시: 길이가 4 이상일 때
+    } else if(cellData < 100000){
+        div.style.fontSize = '43px';
+    }
+
+
+    if (cellData === 2) {
+        div.style.backgroundColor = '#eee4da';
+    } else if (cellData === 4) {
+        div.style.backgroundColor = '#eee1c9';
+    } 
+    else if(cellData === 8 ){
+        div.style.backgroundColor = '#f3b27a';
+    }
+    else if(cellData === 16) {
+        div.style.backgroundColor = '#f69664';
+    }
+    else if(cellData == 32) {
+        div.style.backgroundColor = '#f77c5f';
+    }
+    else if(cellData == 64) {
+        div.style.backgroundColor = '#f75f3b';
+
+    }
+    else if(cellData == 128) {
+        div.style.backgroundColor = '#edd073';
+        div.style.boxShadow = '0 0 30px 10px rgba(243, 215, 116, 0.2380952381), inset 0 0 0 1px rgba(255, 255, 255, 0.1428571429)';
+    }
+    else if(cellData ==256) {
+        div.style.backgroundColor = '#edcc62';
+        div.style.boxShadow = '0 0 30px 10px rgba(243, 215, 116, 0.2380952381), inset 0 0 0 1px rgba(255, 255, 255, 0.1428571429)';
+    }
+    else if(cellData == 512){
+        div.style.backgroundColor = '#edc950';
+        div.style.boxShadow = '0 0 30px 10px rgba(243, 215, 116, 0.3968253968), inset 0 0 0 1px rgba(255, 255, 255, 0.2380952381)';
+    }
+    else if(cellData == 1024) {
+        div.style.backgroundColor = '#edc53f';
+        div.style.boxShadow = '0 0 30px 10px rgba(243, 215, 116, 0.4761904762), inset 0 0 0 1px rgba(255, 255, 255, 0.2857142857)';
+    }
+    else if(cellData == 2048) {
+        div.style.backgroundColor = 'edc22e';
+        div.style.boxShadow = '0 0 30px 10px rgba(243, 215, 116, 0.5555555556), inset 0 0 0 1px rgba(255, 255, 255, 0.3333333333)';
+    }
+
     td.appendChild(div);
     return td;
 }
 
 
-function createTr(rowData) {//쥴을 그려주는 함수 
+const createTr = (rowData) => {//쥴을 그려주는 함수 
     const tr = document.createElement('tr');
     tr.className = 'grid-row';
     rowData.forEach(cellData => {
@@ -140,8 +202,7 @@ function createTr(rowData) {//쥴을 그려주는 함수
     return tr;
 }
 
-
-function createTable(data) {
+const createTable = (data) => {
     const table = document.getElementById('gameTable');
     existLocation(tableData)
     existLocation(tableData) //보통 게임 시작할 때 두개씩 나오드라..
@@ -152,46 +213,153 @@ function createTable(data) {
 }
 
 
-function getCookie(name) { 
-    let matches = document.cookie.match(new RegExp(
-      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-    ));
-    return matches ? decodeURIComponent(matches[1]) : undefined;
-  }
 
-
-
-function setCookie(name, value, options = {}) {
-
-    options = {
-      path: '/', // 경로 지정
-      ...options // 아규먼트로 옵션을 넘겨줬을경우 전개연산자로 추가 갱신
-    };
-  
-    if (options.expires instanceof Date) {
-      options.expires = options.expires.toUTCString(); // 생 Date 객체라면 형식에 맞게 인코딩
+const clickLeft = (tableData) => {
+    if(canMoveLeft(tableData)){
+        for(let y = 0; y<4; y++){ //빈칸 있으면 당기는 용도
+            let newRow = tableData[y].filter(value => value); 
+            for (let x = 0; x < newRow.length - 1; x++) { // 타일 합치기 
+                if (newRow[x] === newRow[x + 1]) {
+                    score += (newRow[x] * 2)
+                    newRow[x] *= 2;
+                    newRow.splice(x + 1, 1);
+                    newRow.push("")
+                }
+            }
+            tableData[y] = newRow.concat(new Array(4 - newRow.length).fill("")); //빈칸만큼 빈 배열 넣어주고 tableDateY 값에 넣어주기
+        }
+        existLocation(tableData)
+        renderTable(tableData)
     }
-  
-    let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
-  
-    for (let optionKey in options) {
-      updatedCookie += "; " + optionKey;
-      let optionValue = options[optionKey];
-      if (optionValue !== true) { // 밸류가 없다면
-        updatedCookie += "=" + optionValue;
-      }
+}
+
+const clickRight = (tableData) => {
+    if(canMoveRight(tableData)){
+        for(let y = 0; y<4; y++){
+            let newRow = tableData[y].filter(value => value); 
+            for(let x=newRow.length-1; x > 0; x--){
+                if (newRow[x] === newRow[x - 1]) {
+                    score += (newRow[x]  * 2)
+                    newRow[x] *= 2;
+                    newRow.splice(x - 1, 1);
+                    newRow.unshift("")
+                }
+            }
+            tableData[y] = new Array(4 - newRow.length).fill("").concat(newRow); //빈칸만큼 빈 배열 넣어주고 tableDateY 값에 넣어주기
+        }
+        existLocation(tableData)
+        renderTable(tableData)
     }
-  
-    document.cookie = updatedCookie; // 새로 갱신
-  }
+}
 
+const clickDown = (tableData) => {
+    if(canMoveDown(tableData)){
+        for (let x = 0; x < 4; x++) {
+            let column = [];
+            for (let y = 0; y < 4; y++) {
+                if (tableData[y][x]) {
+                    column.push(tableData[y][x]);
+                }
+            }
+            for (let i = column.length - 1; i > 0; i--) {
+                if (column[i] === column[i - 1]) { 
+                    score += (column[i] * 2)
+                    column[i] *= 2; 
+                    column.splice(i - 1, 1); 
+                    column.unshift("")
+                }
+            }
+            column = new Array(4 - column.length).fill("").concat(column);
+            for (let y = 0; y < 4; y++) {
+                tableData[y][x] = column[y]; 
+            }
+            
+        }
+        existLocation(tableData)
+        renderTable(tableData)
+    }
+}
 
+const clickUp = (tableData) => {
+    changeCheck = [false,false,false,false]
+    if(canMoveUp(tableData)){
+        for(let x = 0; x<4; x++){
+            let column = [];
+            for (let y = 0; y < 4; y++) {
+                if (tableData[y][x]) {
+                    column.push(tableData[y][x]);
+                }
+            }
+            for (let i = 0; i < column.length - 1; i++) {
+                if (column[i] === column[i + 1]) { 
+                    score += (column[i] * 2)
+                    column[i] *= 2; 
+                    column.splice(i + 1, 1); 
+                    column.push("")
+                }
+            }    
+            column = column.concat(new Array(4 - column.length).fill(""));
+            for (let y = 0; y < 4; y++) {
+                tableData[y][x] = column[y]; 
+            }          
+        }
+        existLocation(tableData)
+        renderTable(tableData)
+    }
+}
+
+const bestCal = () => {
+    if(score>=best){
+        best = score;
+    }
+    return best
+}
+
+const gameWin = () => {
+    div.style.backgroundColor = 'rgba(237, 194, 46, 0.5)'
+}
 
 document.addEventListener('DOMContentLoaded', (event) => { //클릭 이벤트 발생시 동작
     document.getElementById('resetButton').addEventListener('click', function() {
         resetConfirm(tableData);
     });
 });
+
+window.addEventListener("keydown", (e) => {
+    if (e.key==="ArrowRight" || e.key=="d"){
+        e.preventDefault();
+        clickRight(tableData);
+        document.getElementById('user_score').textContent = score
+        bestCal()
+        document.getElementById('user_best').textContent = best
+    }
+    else if(e.key=="ArrowLeft" || e.key=="a"){
+        e.preventDefault();
+        clickLeft(tableData);
+        document.getElementById('user_score').textContent = score
+
+        bestCal()
+        document.getElementById('user_best').textContent = best
+    }
+    else if(e.key=="ArrowDown" || e.key=="s"){
+        e.preventDefault();
+        clickDown(tableData);
+        document.getElementById('user_score').textContent = score
+
+        bestCal()
+        document.getElementById('user_best').textContent = best
+    }
+    else if(e.key=="ArrowUp" || e.key=="w"){
+        e.preventDefault();
+        clickUp(tableData);
+        document.getElementById('user_score').textContent = score
+
+        bestCal()
+        document.getElementById('user_best').textContent = best
+    }
+    
+  });
+
 
 
 createTable(tableData);
